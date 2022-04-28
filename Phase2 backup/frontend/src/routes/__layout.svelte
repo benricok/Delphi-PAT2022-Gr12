3ca8e080aa151b57e757dae87626d1f86782b61e
Benrico Krog <BenricoIT12@pvallei.co.za>
2022-04-28 09:39:07 +0200

    update

--- Phase2 backup/frontend/src/routes/__layout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    // Importing the app.css
-	import '../app.css';
-</script>
-
-<div class="container mx-auto my-6 max-w-lg">
-	<slot />
-</div>
--- Phase2 backup/frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	purge: ['./src/**/*.svelte', './src/**/*.css'],
-	darkMode: false, // or 'media' or 'class'
-	content: [],
-	theme: {
-		extend: {}
-	},
-	plugins: []
-};
